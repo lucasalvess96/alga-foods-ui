aa5febb517841d809e14b9be2218746cb9abfe1a
Lucas Alves <lucasalves.57@hotmail.com>
2025-09-29 17:19:17 -0300

    feat: app.routes

--- alga-foods-ui/src/app/app.routes.ts
+++ alga-foods-ui/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./features/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'welcome',
+    loadComponent: () => import('./features/welcome/welcome.component').then((m) => m.WelcomeComponent),
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/page-not-found/page-not-found.component').then((c) => c.PageNotFoundComponent),
+  },
+];
