30b1ca8aa80985b0ded7aaf323e0131e4c9b5e9c
Lucas Alves <lucasalves.57@hotmail.com>
2025-09-29 15:54:39 -0300

    feat: nome app

--- alga-foods-ui/src/app/app.component.ts
+++ alga-foods-ui/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'alga-foods-ui';
