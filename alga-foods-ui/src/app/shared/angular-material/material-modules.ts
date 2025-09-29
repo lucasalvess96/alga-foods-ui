63d96e9759c1d666ec78170ce0c757e4f2a5d57c
Lucas Alves <lucasalves.57@hotmail.com>
2025-09-29 17:20:42 -0300

    feat: angular material

---
+++ alga-foods-ui/src/app/shared/angular-material/material-modules.ts
@@ -0,0 +1,26 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+export const materialModules = [
+  MatDividerModule,
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatTableModule,
+  MatSortModule,
+  MatPaginatorModule,
+  ReactiveFormsModule,
+  FormsModule,
+];
